f711b02d43d1b750663255d4e80071f283543d49
Vincenzo Vigliotti <v.vigliotti1@studenti.unisa.it>
2020-11-13 19:11:01 +0100

    added pepper_speech_node minor fixes

--- src/pepper_acquire_stream/detector_node.py
+++ src/pepper_acquire_stream/detector_node.py
@@ -4,8 +4,18 @@ import rospy
 from sensor_msgs.msg import Image
 from vision_msgs.msg import Detection2D, Detection2DArray, ObjectHypothesisWithPose
 from detector import Detector
+from pepper_acquire_stream.srv import Say
 import ros_numpy
+from classmap import category_map as classmap
 
+def animated_say_client(labels):
+    rospy.wait_for_service('animated_say')
+    try:
+        animated_say = rospy.ServiceProxy('animated_say', Say)
+        resp = animated_say(labels)
+        return resp.result
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
 
 def rcv_image(msg):
     image = ros_numpy.numpify(msg)
@@ -23,6 +33,10 @@ def rcv_image(msg):
         d.results.append(o)
         message.detections.append(d)
     pub.publish(message)
+    detected_classes = []
+    for c in detections['detection_classes']:
+        detected_classes.append(classmap[c])
+    animated_say_client(detected_classes)
     rospy.loginfo("published")
 
 #Initialize node
@@ -31,7 +45,7 @@ mydetector = Detector(DET_PATH)
 
 rospy.init_node('detector_node')
 
-pub = rospy.Publisher('detection', Detection2DArray, queue_size=2)
+pub = rospy.Publisher('detection', Detection2DArray, queue_size=3)
 si = rospy.Subscriber("take_image_topic", Image, rcv_image)
 
 rospy.spin()
---
+++ src/pepper_acquire_stream/pepper_speech_node.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import rospy
+from naoqi_driver.naoqi_node import NaoqiNode
+from pepper_acquire_stream.srv import Say
+from pepper_acquire_stream.srv import SayResponse
+
+class AnimatedSay(NaoqiNode):
+    def __init__(self):
+        NaoqiNode.__init__(self,'animated_speech')
+        self.connectNaoQi()
+        pass
+        
+    def say(self,data):
+        rospy.loginfo("START: %s" % data.message)
+        #self.speech.say(data.message)
+        rospy.loginfo("END: %s" % data.message)
+        return SayResponse(True)
+
+    def connectNaoQi(self):
+        #self.speech=self.get_proxy("ALAnimatedSpeech")
+        self.s = rospy.Service('animated_say', Say, self.say)
+
+if __name__=="__main__":
+    pub = AnimatedSay()
+    rospy.spin()
