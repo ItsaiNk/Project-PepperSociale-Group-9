9048cd26a405994f737541f65b5e4846c48b92c0
Vincenzo Vigliotti <v.vigliotti1@studenti.unisa.it>
2020-11-23 16:26:11 +0100

    Deleted unused import, deleted bounding box draw phase in rcv_image method

--- src/pepper_group9/detector_node.py
+++ src/pepper_group9/detector_node.py
@@ -2,7 +2,6 @@
 import os
 import rospy
 from pepper_group9.msg import DetectorMessage
-from vision_msgs.msg import Detection2D, ObjectHypothesisWithPose
 from detector import Detector
 from pepper_group9.srv import Say
 import ros_numpy
@@ -20,18 +19,6 @@ def animated_say_client(labels, position):
 def rcv_image(msg):
     image = ros_numpy.numpify(msg.image)
     detections = mydetector(image)
-    # message = Detection2DArray()
-    # for clabel,score,box in zip(detections['detection_classes'], detections['detection_scores'], detections['detection_boxes']):
-    #     d = Detection2D()
-    #     d.bbox.size_x = box[3]-box[1]
-    #     d.bbox.size_y = box[2]-box[0]
-    #     d.bbox.center.x = box[1]+d.bbox.size_x/2
-    #     d.bbox.center.y = box[0]+d.bbox.size_y/2
-    #     o = ObjectHypothesisWithPose()
-    #     o.score = score
-    #     o.id = clabel
-    #     d.results.append(o)
-    #     message.detections.append(d)
     detected_classes = []
     for c in detections['detection_classes']:
         detected_classes.append(classmap[c])
@@ -41,7 +28,6 @@ def rcv_image(msg):
 #Initialize node
 DET_PATH=os.path.join(os.path.dirname(__file__),'efficientdet_d1_coco17_tpu-32')
 mydetector = Detector(DET_PATH)
-
 rospy.init_node('detector_node')
 rospy.loginfo("Detector avviato correttamente")
 si = rospy.Subscriber("take_image_topic", DetectorMessage, rcv_image)
