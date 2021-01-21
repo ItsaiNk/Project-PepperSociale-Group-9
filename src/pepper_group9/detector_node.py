826c375c582b0771c2b5fbad4c9e196f2ad5d884
Vincenzo Vigliotti <vincenzovigliotti94@gmail.com>
2021-01-21 16:47:51 +0100

    changed detector with ssd mobilenet v2 fpnlite 640x640 upgraded pepper_cognitive, detector_node and DetectService in order to check border objects updated graph

--- src/pepper_group9/detector_node.py
+++ src/pepper_group9/detector_node.py
@@ -16,14 +16,14 @@ class DetectorService():
 
     # Constructor of the class
     def __init__(self):
-        DET_PATH=os.path.join(os.path.dirname(__file__),'efficientdet_d1_coco17_tpu-32')
+        DET_PATH=os.path.join(os.path.dirname(__file__),'ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8')
         self.mydetector = Detector(DET_PATH)
         self.service = rospy.Service('detector_service', DetectService, self.detect_handle)
 
     # Receives an image and executes an object detection
     # It uses the classmap in the file classmap.py to map the detected objects with the correct label
     # param: req - The request message containing the Image on which perform the detection
-    # returns: DetectServiceResponse - string[] labels, response of DetectService.srv
+    # returns: DetectServiceResponse - string[] labels, float32[] boxes, response of DetectService.srv
     def detect_handle(self, req):
         image = ros_numpy.numpify(req.image)
         start_time = time.time()
@@ -31,9 +31,13 @@ class DetectorService():
         end_time = time.time()
         rospy.loginfo("DETECTION TIME: " + str((end_time-start_time) % 60) + " SECONDS.")
         detected_classes = []
+        detected_boxes = []
         for c in detections['detection_classes']:
             detected_classes.append(classmap[c])
-        return DetectServiceResponse(detected_classes)
+        for c in detections['detection_boxes']:
+            for v in c:
+                detected_boxes.append(float(v))
+        return DetectServiceResponse(detected_classes, detected_boxes)
 
 if __name__ == "__main__":
     rospy.init_node('detector_node')
@@ -42,5 +46,4 @@ if __name__ == "__main__":
     end_time = time.time()
     rospy.loginfo("DETECTION LOADING TIME: " + str((end_time-start_time) % 60) + " SECONDS.")
     rospy.loginfo("Detector node successfully started")
-    #rospy.sleep(0.01) #necessary in order to let the publish work
     rospy.spin()
--- src/pepper_group9/pepper_cognitive.py
+++ src/pepper_group9/pepper_cognitive.py
@@ -9,18 +9,51 @@ from std_msgs.msg import Bool
 # Class attributes:
 # si - subscriber for the topic "take_image_topic"
 # pub - publisher for the topic "detector_loaded"
+# center_left - dictionary (key: label, value: bounding box) with objects in the left border of the central image
+# center_right - dictionary (key: label, value: bounding box) with objects in the right border of the central image
+# left - dictionary (key: label, value: bounding box) with objects in the right border of the left image
+# right - dictionary (key: label, value: bounding box) with objects in the left border of the right image
 class PepperCognitive():
 
     # Constructor of the class
     def __init__(self):
         self.si = rospy.Subscriber("take_image_topic", DetectorMessage, self.rcv_image)
         self.pub = rospy.Publisher("detector_loaded", Bool, queue_size=1)
+        self.center_left = {}
+        self.center_right = {}
+        self.left = {}
+        self.right = {}
         self.wait_detector_service()
 
     # Wait for the detector service, publish a message on the "detector_loaded" topic when ready
     def wait_detector_service(self):
         rospy.wait_for_service('detector_service')
         self.pub.publish(True)
+
+    # Check if an object is on a border of the image
+    # param: classes - list of labels of objects to examine
+    # param: boxes - bounding box of the objects
+    # param: mode - indicates the image in which the objects appear
+    # return: the dictionaries (key: label, value: bounding box) containing them.
+    def border_items(self, classes, boxes, mode):
+        if mode == "center":
+            th_left = 0.05
+            th_right = 0.95
+        elif mode == "left":
+            th_left = -2.00
+            th_right = 0.95
+        else:
+            th_left = 0.05
+            th_right = 2.00
+
+        left_border_items = {}
+        right_border_items = {}
+        for i in range(len(boxes)):
+            if boxes[i][1] < th_left:
+                left_border_items[classes[i]] = boxes[i]
+            elif boxes[i][3] > th_right:
+                right_border_items[classes[i]] = boxes[i]
+        return left_border_items, right_border_items
 
     # Calls the service "detector_service",
     # which detects objects in an image
@@ -32,10 +65,31 @@ class PepperCognitive():
         try:
             detector_service = rospy.ServiceProxy('detector_service',DetectService)
             resp = detector_service(msg.image)
-            self.text_to_speech_client(resp.labels, position)
+            boxes = [resp.boxes[i:i + 4] for i in range(0, len(resp.boxes), 4)]
+            if position == "center":
+                self.center_left, self.center_right = self.border_items(resp.labels, boxes, position)
+                labels = resp.labels
+            elif position == "left":
+                _, self.left = self.border_items(resp.labels, boxes, position)
+                labels = self.check_border_items(self.center_left, self.left)
+            else:
+                self.right, _ = self.border_items(resp.labels, boxes, position)
+                labels = self.check_border_items(self.center_right, self.right)
+            self.text_to_speech_client(labels, position)
         except rospy.ServiceException as e:
             print("Service call failed: %s"%e)
     
+    # Checks if the same object appears in two adjacent images.
+    # param: center - dictionaries (key: label, value: bounding box) of objects on the common border of the central image
+    # param: side - dictionaries (key: label, value: bounding box) of objects on the common border of the side (left or right) image
+    # return: the updated list of object of the side image, without the repeated elements
+    def check_border_items(self, center, side):
+        for item in side.keys():
+            if item in center:
+                if abs(side[item][0]-center[item][0]) < 0.10 and abs(side[item][2]-center[item][2]) < 0.10:
+                    del side[item]
+        return side.keys()
+
     # Calls the service "text_to_speech_service",
     # which lets Pepper say something you want 
     # param: labels - Specifies the labels of the objects detected by Pepper
